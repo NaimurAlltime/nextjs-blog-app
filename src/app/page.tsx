34855bf04342139f48f4c87a3f60b0a98812fab7
Naimur Rahman <naimur2231@gmail.com>
2024-05-03 11:42:03 -0700

    Show Off All The Fantastic Blogs Using SSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Latestblog from "@/components/LatestBlog/Latestblog";
 
 const HomePage = async () => {
-  const res = await fetch("http://localhost:5000/blogs");
+  const res = await fetch("http://localhost:5000/blogs", {
+    next: {
+      revalidate: 30,
+    },
+  });
   const blogs = await res.json();
   return (
     <>
