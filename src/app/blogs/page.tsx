0ed686c351ebae4021bbecb9eb5bb2718d496129
Naimur Rahman <naimur2231@gmail.com>
2024-05-03 12:31:04 -0700

    Displaying Blog Data Using Redux

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import BlogCard from "@/components/ui/BlogCard";
+import { useGetBlogsQuery } from "@/redux/api/baseApi";
 import { Blog } from "@/types";
 
-const BlogsPage = async () => {
-  const res = await fetch("http://localhost:5000/blogs", {
-    cache: "no-store",
-  });
-  const blogs = await res.json();
+const BlogsPage = () => {
+  //   const res = await fetch("http://localhost:5000/blogs", {
+  //     cache: "no-store",
+  //   });
+  //   const blogs = await res.json();
+
+  const { data: blogs, isLoading, isError, error } = useGetBlogsQuery("");
   // console.log(blogs);
 
   return (
