b5adb0d95b80bbbc490c9c72055aa5ce94e9b72e
Naimur Rahman <naimur2231@gmail.com>
2024-05-03 12:10:01 -0700

    Fetch Dynamic Data Using GenerateStaticParams() For SSG

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -7,13 +7,13 @@ interface BlogId {
   };
 }
 
-// export const generateStaticParams = async () => {
-//   const res = await fetch("http://localhost:5000/blogs");
-//   const blogs = await res.json();
-//   return blogs.slice(0, 3).map((blog: Blog) => ({
-//     blogId: blog.id,
-//   }));
-// };
+export const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+  return blogs.slice(0, 3).map((blog: Blog) => ({
+    blogId: blog.id,
+  }));
+};
 
 const BlogDetailPage = async ({ params }: BlogId) => {
   //   console.log(params);
